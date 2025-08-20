c2c29058370b9853adb89c475e224ec5543d65bd
Dhananjay Kumar <156906791+dj0012@users.noreply.github.com>
2025-08-20 21:29:02 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+// code readability shoud be high
+console.log(3 + 3)
+console.log("dj")
+
+
+let name = "dj" //string datatype
+let age = 18  // numbers value
+let isLoggedIn = false // boolean 
+let state = null // it means empty 
+
+
+// number => 2 to power of 53
+// bigint => 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined =>
+// symbol => for find uniqueness
+
+
+// object =>
+
+console.log(typeof undefined) //undefined
+console.log(typeof null)  //object 
