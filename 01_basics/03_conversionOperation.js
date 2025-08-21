e4d7c3c4c2a1143c03cf4be7e611f2076e13e40b
Dhananjay Kumar <156906791+dj0012@users.noreply.github.com>
2025-08-21 07:34:36 +0000

    conversion of data type

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,5 +24,47 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber =String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************************* Operations **********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " dj"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++; //postfix
+++gameCounter; //prefix
+console.log(gameCounter);
+
+// link to study
+// https://262.ecma-international.org/15.0/index.html?_gl=1*icqviq*_ga*MzQ4MjkxODM0LjE3NTU3NjE0NjM.*_ga_TDCK4DWEPP*czE3NTU3NjE0NjIkbzEkZzAkdDE3NTU3NjE0NjIkajYwJGwwJGgw#sec-abstract-operations
+
