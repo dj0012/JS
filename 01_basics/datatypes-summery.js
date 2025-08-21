396caaf71194cd1eba3c9d88c0c5d0b313691eb2
Dhananjay Kumar <156906791+dj0012@users.noreply.github.com>
2025-08-21 09:06:30 +0000

    datatypes-summery

--- 01_basics/datatypes-summery.js
+++ 01_basics/datatypes-summery.js
@@ -35,3 +35,30 @@ const myFunction = function(){
 console.log(typeof bigNumber);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive),  Heap (Non-Primitive)
+
+let myYoutubename = "djdotcom"
+
+let anothername = myYoutubename
+anothername = "dhananjay"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "dj@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
