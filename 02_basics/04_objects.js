93650cf97836b11b66c54dd59fe0cb69419968af
Dhananjay Kumar <156906791+dj0012@users.noreply.github.com>
2025-08-22 21:01:38 +0000

    json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -46,10 +46,36 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedId'));
+// console.log(tinderUser.hasOwnProperty('isLoggedId'));
+
+
+const course = {
+    coursename: "js",
+    price: "1000",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//    "name": "dhananjay",
+//    "coursename";"js"
+//    "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
