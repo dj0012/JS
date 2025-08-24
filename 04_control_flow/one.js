322df8c79e46db1179b256da537bf2fe57e3b8e7
Dhananjay Kumar <156906791+dj0012@users.noreply.github.com>
2025-08-24 14:30:33 +0000

    if, switch and truthy

---
+++ 04_control_flow/one.js
@@ -0,0 +1,53 @@
+// if 
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// datatype:- <, >=, <=, >=, ==, !=, ===
+
+// const score = 200 
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User Power: ${power}`);   
+// }
+ 
+// console.log(`User Power: ${power}`);
+
+
+// const balance = 1000
+
+// // if (balance > 500) console.log("test"); console.log("test2"); this type code is not good to write
+
+// if (balance < 500) {
+//     console.log("less than");  
+// } else if (balance < 700) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 750"); 
+// } else {
+//     console.log("less than 1200"); 
+// }
+
+
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+
+if (userLoggedIn && debitCard && 2 == 2) {
+    console.log("Allow to buy Course");
+}
+if (loggedInFromGoogle || loggedInFromEmail || true) {
+    console.log("User logged in");
+    
+}
